87b9203ba43cf25eb4effbc5725d96222140bb4d
RoyALeeST <rodrigo.villalobos.sab@gmail.com>
2021-11-08 23:48:39 -0500

    fixed gif and sound sync

--- src/app/components/games/millionaire/board/questions-board/answer/answer-image/answer-image.component.ts
+++ src/app/components/games/millionaire/board/questions-board/answer/answer-image/answer-image.component.ts
@@ -6,37 +6,15 @@ import { MillionaireQuestionsService } from 'src/app/services/games/millionaireQ
   templateUrl: './answer-image.component.html',
   styleUrls: ['./answer-image.component.scss']
 })
-export class AnswerImageComponent implements OnInit, OnChanges {
+export class AnswerImageComponent implements OnInit {
 
   @Input() isAnswerCorrect: boolean;
 
-  imgUrl: string = ""
-  correctAnswerImgUrl: string = 'assets/images/gifs/pepe_answer_reveal_correct.gif';
-  incorrectAnswerImgUrl: string = 'assets/images/gifs/pepe_answer_reveal_incorrect.gif';
-  element: HTMLImageElement = null;
-  constructor(private millionaireQuestionsService: MillionaireQuestionsService) { }
+  @Input() imgUrl: string = "";
+  constructor() { }
 
   ngOnInit(): void {
-    this.millionaireQuestionsService.newQuestionRequested$.subscribe(
-      () => {
-        this.imgUrl = "";
-      }
-    );
-    
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.imgUrl = ""
-    if(this.isAnswerCorrect){
-      this.imgUrl = this.correctAnswerImgUrl;
-    } else {
-      this.imgUrl = this.incorrectAnswerImgUrl;
-    }
-    this.imgUrl = this.imgUrl;
-  }
-
-  doSomething(){
-    console.log("loaded")
-  }
 
 }
